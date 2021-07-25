04f17a30a030715a173ab89adc0671d723e9a37f
Hillary Mongare <hillarymongare70@gmail.com>
2021-07-25 20:04:51 +0300

    login_logout feature

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import UserMixin, LoginManager
 
 # initialize the app 
 app = Flask(__name__, instance_relative_config=True)
@@ -9,7 +10,15 @@ bcrypt = Bcrypt(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = 'thisisasecretkey'
 
-class User(db.Model):
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     firstname = db.Column(db.String(255), nullable=True)
     lastname = db.Column(db.String(255), nullable=True)
--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from app import User, app, db, bcrypt
-from flask import request, json
+from flask import request, json, jsonify, session
+from flask_login import login_user, login_required, logout_user, current_user
 
 @app.route("/")
 def index():
@@ -18,3 +19,32 @@ def createUser():
     db.session.commit()
 
     return {'201': 'User created successfully'}
+
+@app.route("/api/login", methods=["POST"])
+def loginUser():
+
+    request_data = json.loads(request.data)
+    user = User.query.filter_by(username=request_data['username']).first()
+    if user:
+      if bcrypt.check_password_hash(user.password, request_data['password']):
+        login_user(user)
+        session['username'] = user.username
+        return jsonify({"login": True})
+        
+    return jsonify({"login": False})
+
+
+@app.route("/api/getsession", methods=["GET"])
+def check_session():
+  if current_user.is_authenticated:
+    return jsonify({"username": session['username']})
+
+  return jsonify({"login": False})
+
+
+@app.route("/api/logout", methods=["GET"])
+@login_required
+def logout():
+  logout_user()
+  return jsonify({"logout": True})
+

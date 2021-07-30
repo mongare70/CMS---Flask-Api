c582c458b6f795cefd99320f145ed61940c5a817
Hillary Mongare <hillarymongare70@gmail.com>
2021-07-30 11:56:38 +0300

    registration updates

--- app/views.py
+++ app/views.py
@@ -2,23 +2,25 @@ from app import User, app, db, bcrypt
 from flask import request, json, jsonify, session
 from flask_login import login_user, login_required, logout_user, current_user
 
-@app.route("/")
-def index():
-    return "<h1>Hello World</h1>"
 
 @app.route("/api/createUser", methods=["POST"])
 def createUser():
-
     request_data = json.loads(request.data)
-    hashed_password = bcrypt.generate_password_hash(request_data['password'])
-    user = User(username=request_data['username'],
-      email=request_data['email'],
-      password=hashed_password)
+    user = User.query.filter_by(username=request_data['username']).first()
+    
+    if user:
+      return jsonify({ "registered": False})
 
-    db.session.add(user)
-    db.session.commit()
+    else:
+      hashed_password = bcrypt.generate_password_hash(request_data['password'])
+      user = User(username=request_data['username'],
+        email=request_data['email'],
+        password=hashed_password)
 
-    return {'201': 'User created successfully'}
+      db.session.add(user)
+      db.session.commit()
+
+      return {"registered": True}
 
 @app.route("/api/login", methods=["POST"])
 def loginUser():

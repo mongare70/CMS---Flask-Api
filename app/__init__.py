742a7dde21ab4a1ac924476601d3118eb0ee1bc7
Hillary Mongare <hillarymongare70@gmail.com>
2021-09-03 15:05:41 +0300

    slight update

--- app/__init__.py
+++ app/__init__.py
@@ -18,7 +18,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'williamwanger35@gmail.com'
-app.config['MAIL_PASSWORD'] = 'ElDecaps123#'
+app.config['MAIL_PASSWORD'] = 'jxeb wzqi wibn nljh'
 
 cors = CORS(app)
 db = SQLAlchemy(app)
